a593f785928a1dd673d278c67586a81ce91d3f5f
FrankFU323 <tianrui.fu@mail.utoronto.ca>
2024-12-01 20:22:19 -0500

    Add files via upload

--- scripts/05-exploratory_data_analysis.R
+++ scripts/05-exploratory_data_analysis.R
@@ -21,34 +21,33 @@ plot_data <- data %>%
   group_by(causative_agent_1, season) %>% # Group by agent and season for detailed view
   summarise(total_outbreaks = sum(outbreak_count), .groups = "drop")
 
+ggplot(data, aes(x = outbreak_count)) +
+  geom_bar()
 # Create the bar plot
 ggplot(plot_data, aes(x = causative_agent_1, y = total_outbreaks, fill = season)) +
   geom_bar(stat = "identity", position = "dodge") + # Dodge for season-wise bars
-  geom_text(aes(label = total_outbreaks), 
-            position = position_dodge(width = 0.9), 
-            vjust = -0.5, size = 3)
+  geom_text(aes(label = total_outbreaks),
+    position = position_dodge(width = 0.9),
+    vjust = -0.5, size = 3
+  )
 
 ## Predict variable
 ### Causative Agent
 ggplot(data, aes(x = causative_agent_1, fill = causative_agent_1)) +
   geom_bar() +
-  geom_text(stat = 'count', aes(label = ..count..), vjust = -0.5)
+  geom_text(stat = "count", aes(label = ..count..), vjust = -0.5)
 
 ### Season
 ggplot(data, aes(x = season, fill = season)) +
   geom_bar() +
-  geom_text(stat = 'count', aes(label = ..count..), vjust = -0.5)
+  geom_text(stat = "count", aes(label = ..count..), vjust = -0.5)
 
 ### Month
 ggplot(data, aes(x = factor(month, levels = 1:12), fill = factor(month))) +
   geom_bar() +
-  geom_text(stat = 'count', aes(label = ..count..), vjust = -0.5)
+  geom_text(stat = "count", aes(label = ..count..), vjust = -0.5)
 
 ### Outbreak setting
 ggplot(data, aes(x = outbreak_setting, fill = outbreak_setting)) +
   geom_bar() +
-  geom_text(stat = 'count', aes(label = ..count..), vjust = -0.5)
-
-
-
-
+  geom_text(stat = "count", aes(label = ..count..), vjust = -0.5)
